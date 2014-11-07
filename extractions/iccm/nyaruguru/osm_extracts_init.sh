06db6dd153f78e7675a62b94b39b7c8603e6792a
state-hiu <HIU_INFO@state.gov>
2014-11-07 11:56:19 -0500

    nyaruguru

---
+++ extractions/iccm/nyaruguru/osm_extracts_init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#==========##========#
+BIN=/opt/cybergis-scripts.git/bin
+USER=admin
+PASS=geoserver
+GS='http://localhost:8080/geoserver/'
+WS=osm-extracts
+AN=hiu
+AE='HIU_INFO@state.gov'
+TO=360
+RB=~/extractions/repos/iccm/nyaruguru
+#===================#
+EXTENT='rwanda:nyaruguru'
+#Raw Nodes and Ways
+RN=nyaruguru_raw
+REPO=$RB$RN
+rm -fr $REPO
+python $BIN/cybergis-script-geogig-osm-init.py  -v --path $REPO --name $RN --username $USER --password $PASS -gs $GS -ws $WS -to $TO --extent $EXTENT -an $AN -ae $AE --nodes --ways
+#----------#
+#Basic Buildings and Roads
+RN=nyaruguru_basic
+REPO=$RB$RN
+MAPPING='basic:buildings_and_roads'
+rm -fr $REPO
+python $BIN/cybergis-script-geogig-osm-init.py  -v --path $REPO --name $RN --username $USER --password $PASS -gs $GS -ws $WS -to $TO --extent $EXTENT --mapping $MAPPING -an $AN -ae $AE
+#===================#
