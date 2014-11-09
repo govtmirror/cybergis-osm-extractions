fd9ee139eba45969295716854e42237c3739f2ec
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-11-09 13:04:43 -0500

    Create osm_extracts_init.sh

---
+++ extractions/ebola_affected/osm_extracts_init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#==========##========#
+BIN=/opt/cybergis-scripts.git/bin
+USER=admin
+PASS=geoserver
+GS='http://localhost:8080/geoserver/'
+WS=osm-extracts
+AN=hiu
+AE='HIU_INFO@state.gov'
+TO=360
+RB=~/extractions/repos/ebola_affected/baraka/
+#===================#
+EXTENT='drc:baraka'
+#Raw Nodes and Ways
+RN=baraka_raw
+REPO=$RB$RN
+rm -fr $REPO
+python $BIN/cybergis-script-geogig-osm-init.py  -v --path $REPO --name $RN --username $USER --password $PASS -gs $GS -ws $WS -to $TO --extent $EXTENT -an $AN -ae $AE --nodes --ways
+#----------#
+#Basic Buildings and Roads
+RN=baraka_basic
+REPO=$RB$RN
+MAPPING='basic:buildings_and_roads'
+rm -fr $REPO
+python $BIN/cybergis-script-geogig-osm-init.py  -v --path $REPO --name $RN --username $USER --password $PASS -gs $GS -ws $WS -to $TO --extent $EXTENT --mapping $MAPPING -an $AN -ae $AE
+#===================#
