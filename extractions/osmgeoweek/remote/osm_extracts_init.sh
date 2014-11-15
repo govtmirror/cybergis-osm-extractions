383ba9c6b8142d913aaf3398043fe4c8e5a397e4
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-11-15 12:41:15 -0500

    Update osm_extracts_init.sh

--- extractions/osmgeoweek/remote/osm_extracts_init.sh
+++ extractions/osmgeoweek/remote/osm_extracts_init.sh
@@ -13,6 +13,12 @@ RB=~/extractions/repos/osmgeoweek/remote/
 EXTRACTS=osm_extracts_colombo.tsv
 $BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
 #==#
+EXTRACTS=osm_extracts_guinea_1.tsv
+$BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
+#==#
+EXTRACTS=osm_extracts_guinea_2.tsv
+$BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
+#==#
 EXTRACTS=osm_extracts_kathmandu.tsv
 $BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
 #==#
