b19e099a9d5d198ce7d3644a869e85b1c80117ff
state-hiu <HIU_INFO@state.gov>
2014-11-15 17:28:47 -0500

    aggregate script for update and snapshot

---
+++ extractions/osmgeoweek/remote/osm_extracts_update_snapshot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+bash ./osm_extracts_update.sh
+bash ./osm_extracts_snapshot_colombo.sh
+bash ./osm_extracts_snapshot_guinea_1.sh
+bash ./osm_extracts_snapshot_guinea_2.sh
+bash ./osm_extracts_snapshot_kathmandu.sh
+bash ./osm_extracts_snapshot_khulna.sh
+bash ./osm_extracts_snapshot_nyamira.sh
+bash ./osm_extracts_snapshot_philippines.sh
