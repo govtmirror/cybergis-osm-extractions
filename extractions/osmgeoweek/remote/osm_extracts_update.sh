7d73fcf6d7d691f3e0558b6d0ca5efb0b02faf2f
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-11-15 12:32:51 -0500

    Update osm_extracts_update.sh

--- extractions/osmgeoweek/remote/osm_extracts_update.sh
+++ extractions/osmgeoweek/remote/osm_extracts_update.sh
@@ -10,16 +10,17 @@ AE='HIU_INFO@state.gov'
 TO=360
 #===================#
 EXTRACTS=osm_extracts_colombo.tsv
-python  $BIN/cybergis-script-geogig-osm-sync.py false -v -gs $GS -ws $WS --username $USER --password $PASS -an $AN -ae $AE -to $TO --extracts $EXTRACTS
+$BIN/cybergis-script-geogig-osm-sync.py false -v -gs $GS -ws $WS --username $USER --password $PASS -an $AN -ae $AE -to $TO --extracts $EXTRACTS
 #==#
 EXTRACTS=osm_extracts_kathmandu.tsv
-python  $BIN/cybergis-script-geogig-osm-sync.py false -v -gs $GS -ws $WS --username $USER --password $PASS -an $AN -ae $AE -to $TO --extracts $EXTRACTS
+$BIN/cybergis-script-geogig-osm-sync.py false -v -gs $GS -ws $WS --username $USER --password $PASS -an $AN -ae $AE -to $TO --extracts $EXTRACTS
 #==#
 EXTRACTS=osm_extracts_khulna.tsv
-python  $BIN/cybergis-script-geogig-osm-sync.py false -v -gs $GS -ws $WS --username $USER --password $PASS -an $AN -ae $AE -to $TO --extracts $EXTRACTS
+$BIN/cybergis-script-geogig-osm-sync.py false -v -gs $GS -ws $WS --username $USER --password $PASS -an $AN -ae $AE -to $TO --extracts $EXTRACTS
 #==#
 EXTRACTS=osm_extracts_nyamira.tsv
-python  $BIN/cybergis-script-geogig-osm-sync.py false -v -gs $GS -ws $WS --username $USER --password $PASS -an $AN -ae $AE -to $TO --extracts $EXTRACTS
+$BIN/cybergis-script-geogig-osm-sync.py false -v -gs $GS -ws $WS --username $USER --password $PASS -an $AN -ae $AE -to $TO --extracts $EXTRACTS
 #==#
 EXTRACTS=osm_extracts_philippines.tsv
 python  $BIN/cybergis-script-geogig-osm-sync.py false -v -gs $GS -ws $WS --username $USER --password $PASS -an $AN -ae $AE -to $TO --extracts $EXTRACTS
+#==#
