b0af2756b6ef75fc8e16daf1f9b6aebbf76f9dae
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-11-15 14:37:00 -0500

    Update osm_extracts_update.sh

--- extractions/osmgeoweek/remote/osm_extracts_update.sh
+++ extractions/osmgeoweek/remote/osm_extracts_update.sh
@@ -7,7 +7,7 @@ GS='http://localhost:8080/geoserver/'
 WS=osmgeoweek-extracts
 AN=hiu
 AE='HIU_INFO@state.gov'
-TO=360
+TO=840
 #===================#
 EXTRACTS=osm_extracts_colombo.tsv
 $BIN/cybergis-script-geogig-osm-sync.py false -v -gs $GS -ws $WS --username $USER --password $PASS -an $AN -ae $AE -to $TO --extracts $EXTRACTS
