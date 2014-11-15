310fd9a0e6fa1297282464b439472ed49c007742
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-11-15 14:36:48 -0500

    Update osm_extracts_init.sh

--- extractions/osmgeoweek/remote/osm_extracts_init.sh
+++ extractions/osmgeoweek/remote/osm_extracts_init.sh
@@ -7,7 +7,7 @@ GS='http://localhost:8080/geoserver/'
 WS=osmgeoweek-extracts
 AN=hiu
 AE='HIU_INFO@state.gov'
-TO=360
+TO=840
 RB=~/extractions/repos/osmgeoweek/remote/
 #===================#
 EXTRACTS=osm_extracts_colombo.tsv
