9bba005936660c2362465122edace6ddd9e4a9d4
Humanitarian Information Unit <HIU_INFO@state.gov>
2014-11-15 12:32:23 -0500

    Update osm_extracts_init.sh

--- extractions/osmgeoweek/remote/osm_extracts_init.sh
+++ extractions/osmgeoweek/remote/osm_extracts_init.sh
@@ -8,20 +8,20 @@ WS=osm-extracts
 AN=hiu
 AE='HIU_INFO@state.gov'
 TO=360
-RB=~/extractions/repos/ebola/v2/
+RB=~/extractions/repos/osmgeoweek/remote/
 #===================#
 EXTRACTS=osm_extracts_colombo.tsv
-python $BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
+$BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
 #==#
 EXTRACTS=osm_extracts_kathmandu.tsv
-python $BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
+$BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
 #==#
 EXTRACTS=osm_extracts_khulna.tsv
-python $BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
+$BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
 #==#
 EXTRACTS=osm_extracts_nyamira.tsv
-python $BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
+$BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
 #==#
 EXTRACTS=osm_extracts_philippines.tsv
-python $BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
+$BIN/cybergis-script-geogig-osm-init.py  -v --parent $RB --username $USER --password $PASS -gs $GS -ws $WS -to $TO -an $AN -ae $AE --extracts $EXTRACTS
 #==#
